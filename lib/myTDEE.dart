6c41ec29afc35b0185e8a4f8605ac92dab442ec1
Birdie-1 <narueborde@gmail.com>
2024-12-19 16:08:51 +0700

    add to github

---
+++ lib/myTDEE.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TdeeApp extends StatefulWidget {
+  const TDEEApp({super.key});
+
+  @override
+  State TDEEApp> createState() =>  TDEEAppState();
+}
+
+class  TDEEAppState extends State TDEEApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+          appBar: AppBar(
+        title: Text('Person Information'),
+      ) , 
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          children:
+    );
+  }
+}
