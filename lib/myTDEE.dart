c42d029a0a30d123e8658b7076faaf18ce7804d5
Birdie-1 <narueborde@gmail.com>
2024-12-19 16:39:18 +0700

    update

--- lib/myTDEE.dart
+++ lib/myTDEE.dart
@@ -1,23 +1,47 @@
 import 'package:flutter/material.dart';
 
 class TdeeApp extends StatefulWidget {
-  const TDEEApp({super.key});
+  const TdeeApp({super.key});
 
   @override
-  State TDEEApp> createState() =>  TDEEAppState();
+  State<TdeeApp> createState() => TdeeAppState();
 }
 
-class  TDEEAppState extends State TDEEApp> {
+class TdeeAppState extends State<TdeeApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
-          appBar: AppBar(
+    return Scaffold(
+      appBar: AppBar(
         title: Text('Person Information'),
-      ) , 
+      ),
       body: Container(
         margin: EdgeInsets.all(20),
         child: Column(
-          children:
+          children: [
+            SizedBox(height: 20,),
+            Container(width: 100,height: 100,)
+               child: Image.asset
+            Text(
+              'Enter your personal information',
+              style: TextStyle(fontSize: 20, color: Colors.blue),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter your name...',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter your age...',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            // Add more widgets or input fields as needed
+          ],
+        ),
+      ),
     );
   }
 }
